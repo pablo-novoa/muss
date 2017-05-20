12d579ce40a664458934155a6c44bd25c6eb187d
pablo-novoa <novoanpablo@gmail.com>
2017-05-20 19:54:15 -0300

    muss theme create

---
+++ wp-content/themes/muss/functions.php
@@ -0,0 +1,63 @@
+<?php
+//require_once "includes/cmb2init.php";
+
+//enqueue scripts & styles
+add_action('wp_enqueue_scripts', 'muss_enqueueu' );
+//register menues
+add_action('init', 'muss_register_menus' );
+//register main sidebar
+add_action( 'widgets_init', 'muss_sidebar_register' );
+//add theme supports
+add_action('after_setup_theme', 'muss_add_theme_support' );
+//svg format to media library
+add_filter('upload_mimes', 'cc_mime_types');
+
+
+if (!function_exists('cc_mime_types')) {
+function cc_mime_types($mimes) {
+  $mimes['svg'] = 'image/svg+xml';
+  return $mimes;
+}}
+
+if (!function_exists('muss_add_theme_support')) {
+function muss_add_theme_support(){
+	//add thumbnail support
+	add_theme_support( 'post-thumbnails' );
+	//add custom background support
+	add_theme_support( 'custom-background');
+	//add html5 support for search forms, comment forms, comment lists, gallery, and caption
+	add_theme_support( 'html5', array( 'comment-list', 'comment-form', 'search-form', 'gallery', 'caption' ) );
+
+}}
+
+if (!function_exists('muss_register_menus')) {
+function muss_register_menus() {
+	register_nav_menus(
+		array(
+		  'header-menu' => 'Header Menu',
+		  'footer-menu' => 'Footer Menu'
+		)
+	);
+}}
+
+if (!function_exists('muss_enqueueu')) {
+function muss_enqueueu(){
+	//== css ==
+	wp_enqueue_style( 'muss-styles', get_theme_file_uri('style.css') );
+	//== js ==
+	wp_enqueue_script( 'jquery' );
+}}
+
+if (!function_exists('muss_sidebar_register')) {
+function muss_sidebar_register() {
+
+	register_sidebar( array(
+		'name'          => 'Sidebar',
+		'id'            => 'main_sidebar',
+		'before_widget' => '<div id="%1$s" class="widget %2$s main_sidebar_widget">',
+		'after_widget'  => '</div>',
+		'before_title'  => '<h5 class="widget_title">',
+		'after_title'   => '</h5>',
+	) );
+
+}}
---
+++ wp-content/themes/muss/header.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html <?php language_attributes(); ?>>
+
+<head>
+  <meta charset="<?php bloginfo( 'charset' ); ?>">
+  <meta name="viewport" content="width=device-width">
+  <title><?php wp_title('| '.bloginfo('name'), true, 'left'); ?></title>
+
+  <?php wp_head(); ?>
+
+</head>
+
+
+<body <?php body_class(''); ?>>
