2d19d35c1cedf94430e7a1d97006a8afbd654b49
pablo-novoa <novoanpablo@gmail.com>
2017-05-20 20:13:00 -0300

    edit

--- wp-content/themes/muss/functions.php
+++ wp-content/themes/muss/functions.php
@@ -43,9 +43,11 @@ function muss_register_menus() {
 if (!function_exists('muss_enqueueu')) {
 function muss_enqueueu(){
 	//== css ==
-	wp_enqueue_style( 'muss-styles', get_theme_file_uri('style.css') );
+  wp_enqueue_style( 'gridlex', 'https://cdnjs.cloudflare.com/ajax/libs/gridlex/2.3.1/gridlex.min.css' );
+	wp_enqueue_style( 'muss-styles', get_theme_file_uri('style.css'), array('gridlex') );
 	//== js ==
-	wp_enqueue_script( 'jquery' );
+  wp_enqueue_script( 'jquery' );
+	wp_enqueue_script( 'scripts', get_theme_file_uri('js/scripts.js'), array('jquery') );
 }}
 
 if (!function_exists('muss_sidebar_register')) {
