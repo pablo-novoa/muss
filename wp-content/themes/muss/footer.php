496488fc22be763f28fa4f821218fcbc72686287
pablo-novoa <novoanpablo@gmail.com>
2017-05-22 02:52:07 -0300

    footer

--- wp-content/themes/muss/footer.php
+++ wp-content/themes/muss/footer.php
@@ -1,15 +1,17 @@
 </div>
 
 <footer id="page_footer">
-  <div class="main_wrapp footer_row">
-
-  <?php
-    dynamic_sidebar( 'footer_1' );
-    dynamic_sidebar( 'footer_2' );
-    dynamic_sidebar( 'footer_3' );
-    dynamic_sidebar( 'footer_4' );
-    dynamic_sidebar( 'footer_5' );
-  ?>
+  <div class="main_wrapp footer_content">
+    <div class="footer_icon grey_text">&</div>
+    <div class="footer_widgets_wrapp">
+      <?php
+      dynamic_sidebar( 'footer_1' );
+      dynamic_sidebar( 'footer_2' );
+      dynamic_sidebar( 'footer_3' );
+      dynamic_sidebar( 'footer_4' );
+      dynamic_sidebar( 'footer_5' );
+      ?>
+    </div>
 
   </div>
 </footer>
