8ae0a8fe9d5bee3c3b30158349ce8b0390951b3d
pablo-novoa <novoanpablo@gmail.com>
2017-06-22 20:17:32 -0300

    asd

--- wp-content/themes/muss/template-contact.php
+++ wp-content/themes/muss/template-contact.php
@@ -19,9 +19,9 @@ $topBannerImg = get_post_meta( get_the_ID(), '_muss_pageTopBanner_img', true );
 		
 		<div class="contact_page_content">
 		<?php if(have_posts()): while(have_posts()): the_post(); ?>
-			<div class="grey_text">;</div>
-			<h1>comunicate con nosotros <span class="grey_text">,</span></h1>
-			<div>
+			<div class="grey_text contact_coma"><span>;</span></div>
+			<h1 class="contact_title">comunicate con nosotros <span class="grey_text">,</span></h1>
+			<div class="contact_content">
 				<?php the_content(); ?>
 			</div>
 
