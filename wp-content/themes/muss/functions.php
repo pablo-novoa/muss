85fb54469fd61d2a73b9c9ffcc22bc8ac092a4ec
pablo-novoa <novoanpablo@gmail.com>
2017-07-05 03:56:02 -0300

    single prod page

--- wp-content/themes/muss/functions.php
+++ wp-content/themes/muss/functions.php
@@ -199,6 +199,7 @@ function mussSingleProductDetailsHooks(){
 
 	remove_action('woocommerce_after_single_product_summary', 'woocommerce_output_product_data_tabs', 10);
 	remove_action('woocommerce_after_single_product_summary', 'woocommerce_upsell_display', 15);
+	remove_action('woocommerce_single_product_summary', 'woocommerce_template_single_meta', 40);
 
 	add_action('woocommerce_single_product_summary', 'woocommerce_output_product_data_tabs', 40);
 
